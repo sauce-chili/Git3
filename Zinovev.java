db1943fcf3ffc2a17ff5b0abbd37b93cd7b33b00
Zinovev Ivan <izinovev77@gmail.com>
2025-03-12 21:45:54 +0300

    second commit in my branch

--- Zinovev.java
+++ Zinovev.java
@@ -2,6 +2,11 @@ public class Bomb {
 
     private int _radius = 2;
 
+    public int bobrinoGangsterito() {
+        int bombardiroCrocodilo = _radius;
+        return bombardiroCrocodilo * 3;
+    }
+
     public int getRadius() {
         return _radius;
     }
